92058d9eaebf68bb1b9bd42aa17c36d00f86d8d8
Cody Volz <cody.volz@pxl.be>
2023-06-07 12:50:44 +0200

    Test

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const initPassword = 'ZZ727hqUoUGinµùig2PfDkqL2';
+const initPassword = 'ZZ727hqUoUGinµùig2PfDkqL2'
 
-app.use(express.static('public'));
+app.use(bodyParser.json());
 
-app.get('/protected-content', (req, res) => {
-  const { password } = req.query;
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.post('/protected-content', (req, res) => {
+  const { password } = req.body;
 
   if (password === initPassword) {
-    fs.readFile('public/protected-content.html', 'utf8', (err, data) => {
+    fs.readFile('protected-content.html', 'utf8', (err, data) => {
       if (err) {
         res.status(500).json({ message: 'Internal Server Error' });
       } else {
